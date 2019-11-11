aee9d1d8f8a90dee72a1099a1d1610d1fdd3e7f6
Muhammad Alfhi Saputra <alfhisa@student.telkomuniversity.ac.id>
2019-11-12 05:33:51 +0700

    Add files via upload

---
+++ code.r
@@ -0,0 +1,10 @@
+library("openxlsx")
+library("C50")
+
+dataCreditRating <- read.xlsx(xlsxFile = "credit_scoring.xlsx")
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+
+drop_columns <- c("kpr_aktif", "pendapatan_setahun_juta", "risk_rating", "rata_rata_overdue")
+datafeed <- dataCreditRating[ , !(names(dataCreditRating) %in% drop_columns)]
+modelKu <- C5.0(datafeed, as.factor(dataCreditRating$risk_rating))
+summary(modelKu)
