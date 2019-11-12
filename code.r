5a528ebab42710be763ac48d56ac1bd524eca1cc
Muhammad Alfhi Saputra <alfhisa@student.telkomuniversity.ac.id>
2019-11-12 23:11:24 +0700

    Update code.r

--- code.r
+++ code.r
@@ -7,4 +7,4 @@ dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating)
 drop_columns <- c("kpr_aktif", "pendapatan_setahun_juta", "risk_rating", "rata_rata_overdue")
 datafeed <- dataCreditRating[ , !(names(dataCreditRating) %in% drop_columns)]
 modelKu <- C5.0(datafeed, as.factor(dataCreditRating$risk_rating))
-summary(modelKu)
+modelKu
