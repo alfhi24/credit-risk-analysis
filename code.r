4dbf20f48d8b791aeae711668fe12a4909cb18f8
Muhammad Alfhi Saputra <alfhisa@student.telkomuniversity.ac.id>
2020-01-02 19:13:02 +0700

    Update code.r

--- code.r
+++ code.r
@@ -7,3 +7,28 @@ dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating)
 drop_columns <- c("kpr_aktif", "pendapatan_setahun_juta", "risk_rating", "rata_rata_overdue")
 # datafeed <- dataCreditRating[ , !(names(dataCreditRating) %in% drop_columns)]
 modelKu <- C5.0(datafeed, as.factor(dataCreditRating$risk_rating))
+
+
+library("openxlsx")
+library("C50")
+
+#Mempersiapkan data
+dataCreditRating <- read.xlsx(xlsxFile = "credit_scoring.xlsx")
+
+#Mempersiapkan class dan input variables 
+dataCreditRating$risk_rating <- as.factor(dataCreditRating$risk_rating) 
+input_columns <- c("durasi_pinjaman_bulan", "jumlah_tanggungan")
+datafeed <- dataCreditRating[ , input_columns ]
+
+#Mempersiapkan training dan testing set
+set.seed(100) #untuk menyeragamkan hasil random antar tiap komputer
+indeks_training_set <- sample(900, 800)
+
+#Membuat dan menampilkan training set dan testing set
+input_training_set <- datafeed[indeks_training_set,]
+class_training_set <- dataCreditRating[indeks_training_set,]$risk_rating
+input_testing_set <- datafeed[-indeks_training_set,]
+
+#menghasilkan dan menampilkan summary model
+risk_rating_model <- C5.0(input_training_set, class_training_set)
+summary(risk_rating_model)
